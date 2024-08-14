0dd7cdec99868add40961beb32b2381f8330d7a0
Uynaity <u3609460@connect.hku.hk>
2024-08-14 18:26:06 +0800

    Change APP Icon Update README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.uynaity.opendoor"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.0"
+        versionCode = 6
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
